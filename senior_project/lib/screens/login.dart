0a64b40c3e732177316998c96ce316e8d3229079
Maylin Cerf <maylin.catherine.c@gmail.com>
2022-06-20 12:14:37 +0700

    frontend

--- senior_project/lib/screens/login.dart
+++ senior_project/lib/screens/login.dart
@@ -3,7 +3,6 @@ import 'package:get/get.dart';
 import 'package:flutter/gestures.dart';
 import '/colors/colors.dart';
 
-
 class Login extends StatefulWidget {
   const Login({Key? key}) : super(key: key);
 
@@ -22,25 +21,23 @@ class _LoginState extends State<Login> {
   Widget _buildRegisterWith() {
     return Center(
       child: RichText(
-            
-              text: TextSpan(children: [
-                TextSpan(
-                    text: 'Don\'t have account?  ',
-                    style: TextStyle(
-                      color: Colors.black,
-                    ),
-                ),
-                TextSpan(
-                    text: 'Register',
-                    style: TextStyle(
-                      color: Colors.blue,
-                    ),
-                    recognizer: TapGestureRecognizer()
-                      ..onTap = () {
-                         Get.toNamed('/register');
-                      }),
-              ])
-      ),
+          text: TextSpan(children: [
+        TextSpan(
+          text: 'Don\'t have account?  ',
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
+        TextSpan(
+            text: 'Register',
+            style: TextStyle(
+              color: Colors.blue,
+            ),
+            recognizer: TapGestureRecognizer()
+              ..onTap = () {
+                Get.toNamed('/register');
+              }),
+      ])),
     );
   }
 
@@ -54,58 +51,54 @@ class _LoginState extends State<Login> {
       //     style: TextStyle(color: Colors.grey[800]),
       //   ),
       // ),
-      body: 
-      Stack(
-        children:[ 
-      
-      
+      body: Stack(
+        children: [
           Positioned(
-            right: 0,
-            left: 0,
-            child: Container(
-              width: double.maxFinite,
-              height: 250,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.only(bottomLeft: Radius.circular(20.0), bottomRight: Radius.circular(20.0)),
-                color: kLightBlue
-              ),
-            child:Padding(
-              padding: const EdgeInsets.all(30.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  // Container(
-                  //   width: 80,
-                  //   height: 80,
-                  //   child: Image.asset('lib/assets/logo/logo.png')),
-                  Container(
-                    margin: EdgeInsets.fromLTRB(0.0, 20, 0, 0),
-                    child: Text("Login", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30), )),
-                  Text('Login your account in order to use the app.')
-                ],
-              ),
-            ) ,)
-          ),
-      
+              right: 0,
+              left: 0,
+              child: Container(
+                width: double.maxFinite,
+                height: 250,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(20.0),
+                        bottomRight: Radius.circular(20.0)),
+                    color: kLightBlue),
+                child: Padding(
+                  padding: const EdgeInsets.all(30.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      // Container(
+                      //   width: 80,
+                      //   height: 80,
+                      //   child: Image.asset('lib/assets/logo/logo.png')),
+                      Container(
+                          margin: EdgeInsets.fromLTRB(0.0, 20, 0, 0),
+                          child: Text(
+                            "Sign In",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 30),
+                          )),
+                      Text('Login your account in order to use the app.')
+                    ],
+                  ),
+                ),
+              )),
           Positioned(
             top: 150,
             right: 0,
             left: 0,
-            child:             
-            Container(
-
+            child: Container(
               margin: EdgeInsets.all(20),
-
               decoration: BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(30.0)), 
-                 color: Colors.white),
-
+                  borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                  color: Colors.white),
               width: double.infinity,
               height: 300,
               child: ListView(
                 children: <Widget>[
-          
                   Padding(
                     padding: const EdgeInsets.fromLTRB(8.0, 15, 10.0, 8.0),
                     child: TextField(
@@ -139,10 +132,10 @@ class _LoginState extends State<Login> {
                       child: MaterialButton(
                         minWidth: 300.0,
                         height: 50.0,
-                        color: kDarkBlue, 
+                        color: kDarkBlue,
                         shape: const RoundedRectangleBorder(
-                            borderRadius: BorderRadius.all(Radius.circular(15.0)),
-                          ),
+                          borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                        ),
                         onPressed: () {
                           var uname = usernameController.text;
                           var pwd = passwordController.text;
@@ -155,21 +148,20 @@ class _LoginState extends State<Login> {
                           print(_passWord);
                           print(_message);
                           Get.toNamed('/home');
-          
                         },
-                        child: Text('Login', style: TextStyle(
-                            color: Colors.white, fontSize: 15),),
+                        child: Text(
+                          'Login',
+                          style: TextStyle(color: Colors.white, fontSize: 15),
+                        ),
                       ),
                     ),
                   ),
                   // Text('${_userName}')
-                  _buildRegisterWith(), 
+                  _buildRegisterWith(),
                 ],
               ),
             ),
-          
           ),
-          
         ],
       ),
     );
--- senior_project/lib/screens/register.dart
+++ senior_project/lib/screens/register.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:flutter/gestures.dart';
+import '/colors/colors.dart';
 
 class Register extends StatefulWidget {
   const Register({Key? key}) : super(key: key);
@@ -8,7 +11,6 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
-  
   TextEditingController usernameController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
   TextEditingController passwordAgainController = TextEditingController();
@@ -16,109 +18,195 @@ class _RegisterState extends State<Register> {
 
   String _userName = '';
   String _passWord = '';
-  String _passWordAgain = ''; 
-  String _email =''; 
+  String _passWordAgain = '';
+  String _email = '';
   String _message = '';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "Register",
-          style: TextStyle(color: Colors.grey[800]),
-        ),
-      ),
-      body: Container(
-        child: ListView(
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.fromLTRB(8.0, 15, 8.0, 8.0),
-              child: TextField(
-                controller: usernameController,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0)),
-                  labelText: "Username",
-                  hintText: 'Enter your Username',
-                  icon: Icon(Icons.person_outline),
+      backgroundColor: Colors.grey.shade200.withOpacity(0.9999),
+      // appBar: AppBar(
+      //   title: Text(
+      //     "Sign In",
+      //     style: TextStyle(color: Colors.grey[800]),
+      //   ),
+      // ),
+      body: Stack(
+        children: [
+          Positioned(
+              right: 0,
+              left: 0,
+              child: Container(
+                width: double.maxFinite,
+                height: 250,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(20.0),
+                        bottomRight: Radius.circular(20.0)),
+                    color: kLightBlue),
+                child: Padding(
+                  padding: const EdgeInsets.all(30.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      // Container(
+                      //   width: 80,
+                      //   height: 80,
+                      //   child: Image.asset('lib/assets/logo/logo.png')),
+                      Container(
+                          margin: EdgeInsets.fromLTRB(0.0, 40, 0, 0),
+                          child: Text(
+                            "Sign up",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 30),
+                          )),
+                      Text('Sign up your account in order to use the app.')
+                    ],
+                  ),
                 ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(8.0, 8.0, 8.0, 8.0),
-              child: TextField(
-                keyboardType: TextInputType.emailAddress,
-                controller: emailController,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0)),
-                  labelText: "Email",
-                  hintText: 'Enter your Email',
-                  icon: Icon(Icons.email_outlined),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(8.0, 8.0, 8.0, 8.0),
-              child: TextField(
-                // keyboardType: TextInputType.emailAddress,
-                controller: passwordController,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0)),
-                  labelText: "Password",
-                  hintText: 'Enter your Password',
-                  icon: Icon(Icons.key),
-                ),
-              ),
-            ),
-             Padding(
-              padding: const EdgeInsets.fromLTRB(8.0, 8.0, 8.0, 8.0),
-              child: TextField(
-                // keyboardType: TextInputType.emailAddress,
-                controller: passwordAgainController,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0)),
-                  labelText: "Confirm Password",
-                  hintText: 'Enter your Password Again',
-                  icon: Icon(Icons.key),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Center(
-                child: RaisedButton(
-                  onPressed: () {
-                    var uname = usernameController.text;
-                    var pwd = passwordController.text;
-                    var repwd = passwordAgainController.text; 
-                    var email = emailController.text; 
+              )),
+          Positioned(
+            top: 150,
+            right: 0,
+            left: 0,
+            child: Container(
+              margin: EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                  color: Colors.white),
+              width: double.infinity,
+              height: 410,
+              child: ListView(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(8.0, 15, 8.0, 8.0),
+                    child: TextField(
+                      controller: usernameController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0)),
+                        labelText: "Username",
+                        hintText: 'Enter your Username',
+                        icon: Icon(Icons.person_outline),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(8.0, 8.0, 8.0, 8.0),
+                    child: TextField(
+                      keyboardType: TextInputType.emailAddress,
+                      controller: emailController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0)),
+                        labelText: "Email",
+                        hintText: 'Enter your Email',
+                        icon: Icon(Icons.email_outlined),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(8.0, 8.0, 8.0, 8.0),
+                    child: TextField(
+                      // keyboardType: TextInputType.emailAddress,
+                      controller: passwordController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0)),
+                        labelText: "Password",
+                        hintText: 'Enter your Password',
+                        icon: Icon(Icons.key),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(8.0, 8.0, 8.0, 8.0),
+                    child: TextField(
+                      // keyboardType: TextInputType.emailAddress,
+                      controller: passwordAgainController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0)),
+                        labelText: "Confirm Password",
+                        hintText: 'Enter your Password Again',
+                        icon: Icon(Icons.key),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Center(
+                      child: MaterialButton(
+                        minWidth: 300.0,
+                        height: 50.0,
+                        color: kDarkBlue,
+                        shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                        ),
+                        onPressed: () {
+                          var uname = usernameController.text;
+                          var pwd = passwordController.text;
+                          var repwd = passwordAgainController.text;
+                          var email = emailController.text;
 
-                    setState(() {
-                      _userName = uname;
-                      _passWord = pwd;
-                      _passWordAgain = repwd; 
-                      _email = email; 
+                          setState(() {
+                            _userName = uname;
+                            _passWord = pwd;
+                            _passWordAgain = repwd;
+                            _email = email;
 
-                      _message = "username : $uname\nPassword : $pwd";
-                    });
-                    print(_userName);
-                    print(_passWord);
-                    print(_passWordAgain);
-                    print(_email); 
-                    print(_message);
-                    // Get.toNamed('/home');
-
-                  },
-                  child: Text('Register'),
-                ),
+                            _message = "username : $uname\nPassword : $pwd";
+                          });
+                          print(_userName);
+                          print(_passWord);
+                          print(_passWordAgain);
+                          print(_email);
+                          print(_message);
+                          Get.toNamed('/login');
+                        },
+                        child: Text(
+                          'Register',
+                          style: TextStyle(color: Colors.white, fontSize: 15),
+                        ),
+                      ),
+                    ),
+                  ),
+                  // Text('${_userNaSme}'
+                ],
               ),
             ),
-          ],
-        ),
+          ),
+          Positioned(
+              top: 610,
+              right: 0,
+              left: 110,
+              child: Container(
+                child: Text('----------------  More  ---------------- '),
+              )),
+          Positioned(
+              top: 630,
+              right: 0,
+              left: 140,
+              child: Container(
+                // margin: EdgeInsets.all(10),
+                // decoration: BoxDecoration(
+                //     borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                //     color: Colors.white),
+                width: 10,
+                height: 70,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    IconButton(
+                        icon: Icon(Icons.email, size: 40), onPressed: () {}),
+                    IconButton(
+                        icon: Icon(Icons.facebook, size: 40), onPressed: () {})
+                  ],
+                ),
+              ))
+        ],
       ),
     );
   }
